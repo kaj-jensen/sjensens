a03535ddcc0f65887bad9959687bcd1f8da4c451
Kaj Jensen <kajjensen@MacBook-Air-tilhrende-Kaj.local>
2026-08-18 08:01:42 +0200

    Add GA4 tracking

---
+++ analytics.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = window.gtag || function gtag() {
+  window.dataLayer.push(arguments);
+};
+window.gtag("js", new Date());
+window.gtag("config", "G-4Z414XJ7GJ", { anonymize_ip: true });
+
+const analyticsScript = document.createElement("script");
+analyticsScript.async = true;
+analyticsScript.src = "https://www.googletagmanager.com/gtag/js?id=G-4Z414XJ7GJ";
+document.head.appendChild(analyticsScript);
